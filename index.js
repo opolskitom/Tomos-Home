cd06cc906e144157664a5f941114448a6eb5ec03
Thomas Opolski <opolskitom@gmail.com>
2020-04-06 00:14:25 -0400

    Commit 4/6
    Started work on the navigation bar, added animations for title, and many minor changes to current layout.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+var soundEnabled = true;
 
+//Sound Button
+$(".unpressed").children(".fa-volume-mute.sound-icon").hide();
 $(".unpressed").click(function() {
+  soundEnabled = !soundEnabled;
+  $(this).toggleClass("pressed").toggleClass("unpressed");
+  $(this).children(".fa-volume-mute.sound-icon").toggle();
+  $(this).children(".fa-volume-up.sound-icon").toggle();
   if ($(this).hasClass("unpressed")) {
-    $(this).addClass("pressed").removeClass("unpressed");
+    $(this).children(".sound-icon").css("color","#d6d6d6");
   } else {
-    $(this).addClass("unpressed").removeClass("pressed");
+    $(this).children(".sound-icon").css("color","#858585");
   }
 });
